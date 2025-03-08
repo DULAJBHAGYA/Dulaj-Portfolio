d4ba92a26fd2b45cac83bf78246ac28c36ac6d0f
Dulaj Bhagya <dulajupananda@gmail.com>
2025-03-08 12:25:04 +0530

    fifth commit
    Developed blogs section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,6 +7,7 @@ import CheckIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
 import GithubIcon from "@/assets/icons/github.svg";
 import GrainImage from "@/assets/images/grain.jpg";
+import { SectionHeader } from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
@@ -48,17 +49,11 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
-            Real-world Results
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-          Featured Projects
-        </h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4  max-w-md mx-auto">
-          See how I transformed conceptd into engaging digital experiences.
-        </p>
+        <SectionHeader 
+              eyebrow="Real-world Results"
+              title="Featured Projects"
+             description="See how I transformed concepts into engaging digital expiriences."/>
+        
 
         <div className="flex flex-col gap-20 mt-10 md:mt20">
           {portfolioProjects.map((project) => (
