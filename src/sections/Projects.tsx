9daaea07ca5ac688e76028a09286ab953c2339d7
Dulaj Bhagya <dulajupananda@gmail.com>
2025-03-23 18:02:33 +0530

    23rd march second commit
    Developed tape section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -56,10 +56,15 @@ export const ProjectsSection = () => {
         
 
         <div className="flex flex-col gap-20 mt-10 md:mt20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl
+               after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none sticky" 
+               style={{
+                  top: `calc(64px + ${projectIndex * 50}px`
+                }
+               }
             >
               <div className="absolute inset-0 -z-10 opacity-5" 
               style={{
